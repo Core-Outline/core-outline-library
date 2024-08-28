eaac53c90ce95bc6a8a3f82ec21311ece372aa26
TomiTsuma <tommytsuma7@gmail.com>
2024-08-28 17:05:00 +0300

    File upload set up

--- src/aws-config.js
+++ src/aws-config.js
@@ -1,14 +1,16 @@
 import AWS from 'aws-sdk';
-import dotenv from 'dotenv';
 
-// Load environment variables from .env file
-dotenv.config();
 
-// Configure AWS with environment variables
+// AWS.config.update({
+//   accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
+//   secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
+//   region: process.env.REACT_APP_AWS_REGION
+// });
+
 AWS.config.update({
-  accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
-  region: process.env.REACT_APP_AWS_REGION
+  accessKeyId: "AKIA5FTZC7FK6YDB7IWD",
+  secretAccessKey: "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",
+  region: "us-west-2"
 });
 
 export const s3 = new AWS.S3();
